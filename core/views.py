7841ba1023181c1dd52368b5979b697d4453f655
LeoHeringer <leonardoheringer2016@hotmail.com>
2024-07-19 02:44:22 -0300

    models

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+@api_view(['POST'])
+def create_client(request):
+
+    name = request.data.get("name")
+    cnpj = request.data.get("cnpj")
+
+
+    if not name:
+        return Response({'message': "invalid str"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    if not cnpj:
+        return Response({'message': "invalid str"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if Client.objects.filter(name=name, cnpj=cnpj).exists():
+        return Response({"message":"exists"}, status=status.HTTP_400_BAD_REQUEST)
